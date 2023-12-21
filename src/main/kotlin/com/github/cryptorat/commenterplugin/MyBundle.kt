58b7cbb28c7e9eaaceb76c7aad543ac93d4dbc42
GitHub Action <action@github.com>
2023-12-21 18:01:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/cryptorat/commenterplugin/MyBundle.kt
+++ src/main/kotlin/com/github/cryptorat/commenterplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cryptorat.commenterplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cryptorat/commenterplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cryptorat/commenterplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.cryptorat.commenterplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cryptorat.commenterplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
